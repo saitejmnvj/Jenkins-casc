bc6771cdd7e3cebe41a906121013e761e06aab8e
Saitej Matukumalli <saitejmnvj@gmail.com>
2021-06-23 09:22:04 +0530

    Removingt execute.sh

--- casc/execute.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-/usr/local/bin/install-plugins.sh < /usr/share/jenkins/plugins.txt
-
-
-job-dsl-plugin:1.77
-mask-passwords-plugin:2.13
-
- j(e6n)k inCso.uilod                                                      
- https://updates.jenkins.io/dynamic-2.291//latest/active-directory.hpi
